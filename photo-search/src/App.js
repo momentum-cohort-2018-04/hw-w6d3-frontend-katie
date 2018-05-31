8f7de1da359a76b46db393e52df2b0da6dd06a17
kcbobbe <kcbobbe@gmail.com>
2018-05-31 16:16:40 -0400

    Got basic image search to work

--- photo-search/src/App.js
+++ photo-search/src/App.js
@@ -7,38 +7,60 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      images: []
+      images: [],
+      text: ''
     }
   }
 
 
   render() {
+    console.log(this.state.text)
     return (
       <div className="App">
-        <p>{this.state.images}</p>
-        <button onClick={this.getimages.bind(this)}>Read More!</button>
+        {/* <p>{this.state.images.user}</p>
+        <img src = {this.state.images.previewURL}/>
+        <p>{this.state.images.tags}</p> */}
+        <Image image={this.state.images} q={this.state.text}/>
+        <textarea className ='search-box' placeholder="Search For Images" name="search" id="search" onChange = {this.updateText.bind(this)}/>
+        <button type="submit" className="button" onClick={this.getimages.bind(this)}>Search For Images!</button>
       </div>
     );
   }
 
 
   getimages(){
-    request.get(`https://pixabay.com/api/?key=${process.env.REACT_APP_SECRET_CODE}&q=('red')`)
+    request.get(`https://pixabay.com/api/?key=${process.env.REACT_APP_SECRET_CODE}&q=('${this.state.text}')`)
     .then(response => {
        this.setState({
-        images : response.body.hits[0].user
+        images : response.body.hits[0]
       })
-      console.log(this.state.images)
+      console.log(this.state.images.previewURL)
   })
 // }
 }
+updateText (event) {
+  this.setState({text: event.target.value})
+}
+
 }
 
 class Image extends Component {
   constructor () {
     super()
-    this.state = {
-    }
+    
+}
+  render(){
+    console.log(this.props)
+  return (
+    <div className="image-container">
+        <p>yo</p>
+        <p>{this.props.image.user}</p>
+        <img src = {this.props.image.previewURL}/>
+        <p>{this.props.image.tags}</p>
+    </div>
+  )
+  
+}
 }
 
 export default App;
