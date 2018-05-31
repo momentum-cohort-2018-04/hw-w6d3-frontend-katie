f065bab35a37a6b898e872d509386465d8455da8
kcbobbe <kcbobbe@gmail.com>
2018-05-31 17:53:00 -0400

    Got multiple search results to display

--- photo-search/src/App.js
+++ photo-search/src/App.js
@@ -14,15 +14,21 @@ class App extends Component {
 
 
   render() {
-    console.log(this.state.text)
+    console.log('image-state', this.state.images)
+    const images = this.state.images
+    const text = this.state.text
     return (
       <div className="App">
+      <h1>IMAGE SEARCH</h1>
+      <input className ='search-box' placeholder="Search For Images" name="search" id="search" onChange = {this.searchTextUpdate.bind(this)}/>
+        <button type="submit" className="button" onClick={this.getimages.bind(this)}>Search For Images!</button>
         {/* <p>{this.state.images.user}</p>
         <img src = {this.state.images.previewURL}/>
         <p>{this.state.images.tags}</p> */}
-        <Image image={this.state.images} q={this.state.text}/>
-        <textarea className ='search-box' placeholder="Search For Images" name="search" id="search" onChange = {this.updateText.bind(this)}/>
-        <button type="submit" className="button" onClick={this.getimages.bind(this)}>Search For Images!</button>
+        
+        {images.map(function(image){
+          return <Image image={image} q={text}/>
+        })}
       </div>
     );
   }
@@ -32,13 +38,13 @@ class App extends Component {
     request.get(`https://pixabay.com/api/?key=${process.env.REACT_APP_SECRET_CODE}&q=('${this.state.text}')`)
     .then(response => {
        this.setState({
-        images : response.body.hits[0]
+        images : response.body.hits
       })
       console.log(this.state.images.previewURL)
   })
 // }
 }
-updateText (event) {
+searchTextUpdate (event) {
   this.setState({text: event.target.value})
 }
 
@@ -53,7 +59,6 @@ class Image extends Component {
     console.log(this.props)
   return (
     <div className="image-container">
-        <p>yo</p>
         <p>{this.props.image.user}</p>
         <img src = {this.props.image.previewURL}/>
         <p>{this.props.image.tags}</p>
