353a20ec6fd53b12d6da31e9deb476a0c24d8359
kcbobbe <kcbobbe@gmail.com>
2018-06-02 17:54:59 -0400

    improved expansion feature

--- photo-search/src/App.js
+++ photo-search/src/App.js
@@ -19,17 +19,23 @@ class App extends Component {
     const text = this.state.text
     return (
       <div className="App">
-      <h1>IMAGE SEARCH</h1>
-      <input className ='search-box' placeholder="Search For Images" name="search" id="search" onChange = {this.searchTextUpdate.bind(this)}/>
-        <button type="submit" className="button" onClick={this.getimages.bind(this)}>Search For Images!</button>
+      
         {/* <p>{this.state.images.user}</p>
         <img src = {this.state.images.previewURL}/>
         <p>{this.state.images.tags}</p> */}
+        <div className = "film-border">
         <div className= "image-container">
         {images.map(function(image){
           return <Image image={image} q={text}/>
         })}
         </div>
+        </div>
+        {/* <div className="slide-container">
+          <input type="range" min="1" max="100" value="50" className="slider"/>
+        </div> */}
+        <h1 className = "title">THE FILM REEL PHOTO SEARCH</h1>
+      <input className ='search-box' placeholder="Search For Images" name="search" id="search" onChange = {this.searchTextUpdate.bind(this)}/>
+        <button type="submit" className="button" onClick={this.getimages.bind(this)}>Search For Images!</button>
       </div>
     );
   }
@@ -70,16 +76,29 @@ expandPic(){
     console.log(this.props)
   return (
     <div>
-    <div className="imageWithInfo">
-        <p>Contributor: <strong>{this.props.image.user}</strong></p>
-        <img onClick ={this.expandPic.bind(this)} src = {this.props.image.previewURL}/>
-        <p>Tags: <strong>{this.props.image.tags}</strong></p>
-    </div>
-    {this.state.expanded && <div>
+    
+    {this.state.expanded ?(
+       <div>
     {/* img on click goes to expandPic */}
-    <img onClick = {this.expandPic.bind(this)} className = 'big-photo' src = {this.props.image.largeImageURL}/>
-    <BigPhoto expanded = {this.state.expanded}/>
-    </div>}
+      <img onClick = {this.expandPic.bind(this)} className = 'big-photo' src = {this.props.image.largeImageURL}/>
+      <div className= "more-info">
+        <div className = "src-link">
+          <a href={this.props.image.pageURL}>Link</a>
+        </div>
+        <div className= "more-info-item">Contributor: <strong>{this.props.image.user}</strong></div>
+        <div className= "more-info-item">Likes: <strong>{this.props.image.likes}</strong></div>
+        <div className= "more-info-item">Downloads: <strong>{this.props.image.downloads}</strong></div>
+        {/* <p>Tags: <strong>{this.props.image.tags}</strong></p> */}
+      </div>
+      {/* <BigPhoto expanded = {this.state.expanded}/> */}
+      </div>
+    ) : (
+      <div className="imageWithInfo">
+        {/* <p>Contributor: <strong>{this.props.image.user}</strong></p> */}
+        <img className = "thumbnail" onClick ={this.expandPic.bind(this)} src = {this.props.image.previewURL}/>
+        {/* <p>Tags: <strong>{this.props.image.tags}</strong></p> */}
+    </div>
+    )}
     </div>
   )
   
